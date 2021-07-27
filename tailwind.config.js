fb2e3405501cb28fe50b1ea9648a7f92a0873601
David Salazar <davidjsalazarmoreno@gmail.com>
2021-07-27 16:24:27 -0500

    adds tailwind + styles components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-blue-1': '#0095DA',
+        'brand-blue-2': '#005888',
+        'brand-blue-3': '#83F2F2',
+        'brand-gray-1': '#CDCDCD',
+        'brand-gray-2': '#B4B4B4',
+      },
+    },
   },
   variants: {
     extend: {},
